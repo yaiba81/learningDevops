753069b543409b88958dc5c86060cb72a2c208c8
Jonathan <han_082781@yahoo.com>
2019-02-25 22:40:55 +0800

    Initial for our first spring

---
+++ src/main/java/com/learningDevOps/LearningDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.learningDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDevOpsApplication.class, args);
+	}
+
+}
