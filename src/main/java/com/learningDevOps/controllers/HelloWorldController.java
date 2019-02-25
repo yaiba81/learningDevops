61b2541411b53800b7806075260310ecf9793591
Jonathan <han_082781@yahoo.com>
2019-02-25 23:10:28 +0800

    Hello World

---
+++ src/main/java/com/learningDevOps/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.learningDevOps.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return "index";
+	}
+}
