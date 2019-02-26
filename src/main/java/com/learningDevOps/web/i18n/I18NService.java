ef9a62b202094eb3a6b080f29d03aa6bec5c7435
Jonathan <han_082781@yahoo.com>
2019-02-26 23:29:44 +0800

    Added i18n to skeleton project

---
+++ src/main/java/com/learningDevOps/web/i18n/I18NService.java
@@ -0,0 +1,33 @@
+package com.learningDevOps.web.i18n;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class I18NService {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	/**
+	 * Returns a message for the given message id and the default locale in the session context
+	 * @param messageId The key to the message resource file 
+	 **/
+	public String getMessage(String messageId) {
+		Locale locale = LocaleContextHolder.getLocale();
+		return getMessage(messageId, locale);
+	}
+	
+	/**
+	 * Returns a message for the given message id and locale
+	 * @param messageId The key to the message resource file
+	 * @param locale The Locale
+	 **/
+	public String getMessage(String messageId, Locale locale) {
+		return messageSource.getMessage(messageId, null, locale);
+	}
+}
--- src/test/java/com/learningDevOps/LearningDevOpsApplicationTests.java
+++ src/test/java/com/learningDevOps/LearningDevOpsApplicationTests.java
@@ -2,15 +2,27 @@ package com.learningDevOps;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.learningDevOps.web.i18n.I18NService;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LearningDevOpsApplicationTests {
 
+	@Autowired
+	private I18NService i18nService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("The Actual and Expected don't match", expectedResult, actual);
 	}
 
 }
