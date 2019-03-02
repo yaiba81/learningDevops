af7ca6566a43a8aa441b6dd50a11e86c61d4af1a
Jonathan <han_082781@yahoo.com>
2019-03-02 17:12:31 +0800

    Added form to contact us page

---
+++ src/main/java/com/learningDevOps/web/domain/frontend/FeedbackPojo.java
@@ -0,0 +1,71 @@
+package com.learningDevOps.web.domain.frontend;
+
+import java.io.Serializable;
+
+public class FeedbackPojo implements Serializable  {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String email,
+				   firstName,
+				   lastName,
+				   feedback;
+
+	public FeedbackPojo() {
+		super();
+	}
+
+	public FeedbackPojo(String email, String firstName, String lastName, String feedback) {
+		super();
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.feedback = feedback;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("FeedbackPojo{");
+		sb.append("email='").append(email).append('\'');
+		sb.append(", firstName='").append(firstName).append('\'');
+		sb.append(", lasName='").append(lastName).append('\'');
+		sb.append(", feedback='").append(feedback).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	
+
+}
